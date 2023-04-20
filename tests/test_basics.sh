b79d268e14e41f57a3cdb6c512ffd4405e5883d4
Fredrik Öhrström <oehrstroem@gmail.com>
2023-04-20 20:14:31 +0200

    Work on build.

--- tests/test_basics.sh
+++ tests/test_basics.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 #
-#    Copyright (C) 2016-2020 Fredrik Öhrström
+#    Copyright (C) 2016-2023 Fredrik Öhrström
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
